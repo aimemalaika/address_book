496db46041a53ca6578c7c73e177c6e864a190f3
Aime Malaika <aimemalaika1995@gmail.com>
2021-08-23 03:14:36 +0200

    added fixtures email and phone

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # address_book
 
 - clone the repository
+
   `git clone https://github.com/aimemalaika/address_book.git`
   
 - install dependency 
+
   `composer install`
  
- - configure credential for database in .env file
+ - configure credential for database in .env file & create database
+
+  DATABASE_URL="mysql://`db_username`:`db_password`@127.0.0.1:3306/`db_name`?`server_version`"
+
+  `php bin/console doctrine:database:create`
  
  - run migrations
+
   `php bin/console doctrine:migrations:migrate`
  
  - insert datas
+
   `php bin/console doctrine:fixtures:load`
   
  - run server
+
   `php bin/console server:run`
+
+- App Logins
+
+  username = `aimemalaika`
+  password = `Aime1995@`
+
--- src/DataFixtures/AddessesFixture.php
+++ src/DataFixtures/AddessesFixture.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Contacts;
+use App\Entity\Emails;
+use App\Entity\Telephone;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -12,13 +14,25 @@ class AddessesFixture extends Fixture
     public function load(ObjectManager $manager)
     {
         $facker = Factory::create('en_US');
-        for ($i=0; $i < 300; $i++) { 
+        for ($i=0; $i < 200; $i++) { 
             $contact = new Contacts();
             $firstName = (($i % 3) == 0) ? $facker->firstNameMale : $facker->firstNameFemale;
             $lastName = $facker->lastName;
             $contact->setFirstname($lastName)
                     ->setLastname($firstName);
             $manager->persist($contact);
+
+            for($j= 0; $j < rand(3,8); $j++){
+                $email = new Emails();
+                $email->setEmailaddress($facker->freeEmail)
+                        ->setContact($contact);
+                $manager->persist($email);
+
+                $tel = new Telephone();
+                $tel->setTelephone($facker->e164PhoneNumber)
+                        ->setContact($contact);
+                $manager->persist($tel);
+            }
         }
 
         $manager->flush();
